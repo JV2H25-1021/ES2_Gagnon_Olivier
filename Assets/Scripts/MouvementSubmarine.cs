3dfd6c4111fc2d5c2b68a451386e7c969a0ff13a
Eaulitviez <2077834@cegepmontpetit.ca>
2025-02-22 18:48:02 -0500

    tentative animation inverse

--- Assets/Scripts/MouvementSubmarine.cs
+++ Assets/Scripts/MouvementSubmarine.cs
@@ -12,6 +12,7 @@ public class MouvementSubmarine : MonoBehaviour
     private Vector3 directionInput;
     private Animator _animator;
    [SerializeField] private float _vitesseSubmarine;
+    private float vitesseNormale = 1f;
 
 
     // Start is called before the first frame update
@@ -19,14 +20,15 @@ public class MouvementSubmarine : MonoBehaviour
     {
         _rb = GetComponent<Rigidbody>();
         _animator = GetComponent<Animator>();
-        
+        _vitesseSubmarine = vitesseNormale;
+
+
     }
     void OnMouvement(InputValue directionBase)
     {
         Vector2 directionAvecVitesse = directionBase.Get<Vector2>() * _vitesseSubmarine;
         directionInput = new Vector3(0f, directionAvecVitesse.y, directionAvecVitesse.x);
         _animator.SetFloat("DeplacementZ", directionInput.magnitude);
-
 
     }
 
@@ -40,5 +42,17 @@ public class MouvementSubmarine : MonoBehaviour
 
 
     }
+    void OnAccelerer(InputValue bouton)
+    {
+        if (bouton.isPressed)
+        {
+            _vitesseSubmarine = vitesseNormale * 2;
 
+        }
+        else
+        {
+            _vitesseSubmarine = vitesseNormale;
+        }
+
+    }
 }
